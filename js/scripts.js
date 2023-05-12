b100480ae5f8730007613ef972b550d782ce6c31
Ilia Green <tim2015web@gmail.com>
2023-05-12 15:13:22 +0300

    Burger menu js - ok

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,26 @@
-//Hamburger menu
+//Hamburger menu----------
+let barElement = document.querySelectorAll(".bar");
+let barPanelElement = document.querySelector(".bar-panel");
+let barStatus = false; //Panel display status
 
+function closeBarPanel() { //Hide the panel
+   barStatus = false;
+   barPanelElement.style = 'display: none;';
+}
 
+barPanelElement.addEventListener("click", closeBarPanel);
 
-//Statistica animation
-let statisticsElement = document.querySelectorAll(".count"); //Statistics elements
+barElement.forEach(e => e.addEventListener("click", function () {
+   if (barStatus == false) {
+      barStatus = true;
+      barPanelElement.style = 'display: block;';
+   } else {
+      closeBarPanel();
+   }
+}));
+
+//Statistic animation----------
+let statisticsElement = document.querySelectorAll(".count");
 let elementArray = [];
 let numbersArray = [];
 let statisticsCountTimes = 30; //The count of repetitions
@@ -16,7 +33,7 @@ statisticsElement.forEach(e => {
 window.addEventListener("scroll", function statisticsAnimation() {
    if (statisticsElement[0].getBoundingClientRect().top < (window.innerHeight - 100)) {
       window.removeEventListener("scroll", statisticsAnimation);
-      
+
       let statisticsTimer = setInterval(timerFunction, 50);
 
       function timerFunction() {
@@ -28,6 +45,6 @@ window.addEventListener("scroll", function statisticsAnimation() {
          //Stop the timer
          statisticsCountTimes--;
          if (statisticsCountTimes == 0) { clearInterval(statisticsTimer) };
-      }      
+      }
    }
 });
